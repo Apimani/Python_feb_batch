7abd90ef6f151efd83f7086988f3f211742ebe8a
Sreeni <kattubadi426@gmail.com>
2024-03-12 20:35:05 +0530

    new commit

--- exceptions_handling/exception_part2.py
+++ exceptions_handling/exception_part2.py
@@ -1,34 +1,48 @@
-import logging
-import pandas as pd
-a = 10
-b = 20
-
-print("Hello world")
-try:
-    print("division of two number", a/b)
-except ZeroDivisionError as e:
-    print("division is not possible with zero", e)
-    logging.error("Zero division")
-except TypeError as e:
-    print("division is not possible with un supported type", e)
-    logging.error("type error")
-except :
-    print("This is default exception")
-    logging.error("default", a,b)
-
-try:
-    df = pd.read_csv(r"C:\Users\A4952\Downloads\Python_feb_batch-20240218T160531Z-001\Python_feb_batch\Files\Contact_info2.csv", nrows=10)
-    print(df.head(2))
-except FileNotFoundError as e:
-    df = pd.read_csv(
-        r"C:\Users\A4952\Downloads\Python_feb_batch-20240218T160531Z-001\Python_feb_batch\Files\Contact_info.csv",
-        nrows=10)
-    print(df.head(2))
-    print("file not found", e)
-    logging.error("File not found")
-except:
-    print("default")
+# import logging
+# import pandas as pd
+# a = 10
+# b = 20
+#
+# print("Hello world")
+# try:
+#     print("division of two number", a/b)
+# except ZeroDivisionError as e:
+#     print("division is not possible with zero", e)
+#     logging.error("Zero division")
+# except TypeError as e:
+#     print("division is not possible with un supported type", e)
+#     logging.error("type error")
+# except :
+#     print("This is default exception")
+#     logging.error("default", a,b)
+#
+# try:
+#     df = pd.read_csv(r"C:\Users\A4952\Downloads\Python_feb_batch-20240218T160531Z-001\Python_feb_batch\Files\Contact_info2.csv", nrows=10)
+#     print(df.head(2))
+# except FileNotFoundError as e:
+#     df = pd.read_csv(
+#         r"C:\Users\A4952\Downloads\Python_feb_batch-20240218T160531Z-001\Python_feb_batch\Files\Contact_info.csv",
+#         nrows=10)
+#     print(df.head(2))
+#     print("file not found", e)
+#     logging.error("File not found")
+# except:
+#     print("default")
 
 #df = pd.read_csv(r"C:\Users\A4952\Downloads\Python_feb_batch-20240218T160531Z-001\Python_feb_batch\Files\Contact_info.csv", nrows=5)
+
+try:
+   print("establishing connection to db")
+   print("query", 'test')
+   print("reading data")
+
+except:
+    print("table not found")
+
+finally:
+    print("connection close")
+
+
+
 
 print("End of python code")
