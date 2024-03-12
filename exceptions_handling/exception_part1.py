e847acf5cc118f122d650810157102515bf74633
Sreeni <kattubadi426@gmail.com>
2024-03-12 09:08:03 +0530

    new commit

---
+++ exceptions_handling/exception_part1.py
@@ -0,0 +1,19 @@
+
+a = 10
+b = 20
+#requirement : add two numbers and store in c variable and print c varible
+
+c = a+b #(  a-b inplace of + team has implemented -) unit testing/function testing
+print(c) # Print(c) syntax error and can be identified during unit testing
+
+print("end of code")
+
+print(input("enter number"))
+
+# age =20
+# if age > 20:#( inplace of + team has implemented -)
+# print("eligible for vote")
+# else:
+#     print("not eligible")
+
+import Cx_oracle
--- logger/logger2.py
+++ logger/logger2.py
@@ -1,18 +1,29 @@
 import pandas as pd
 
 a=10
+b=0
 
 
-# try:
-#     print("a divide by b is", a/b)
-# except ZeroDivisionError as e:
-#     print("Can divide with zero",e)
-# except TypeError as e:
-#     print("Type of a or b is not interger/numeric", e)
-# except  NameError as e:
-#     print("a or b is not defined", e)
-#
-# print("This is end")
+try:
+    print("a divide by b is", a/b)
+except ZeroDivisionError as e:
+    print("Can divide with zero",e)
+except TypeError as e:
+    print("Type of a or b is not interger/numeric", e)
+except  NameError as e:
+    print("a or b is not defined", e)
+
+print("This is end")
+
+
+
+
+
+
+
+
+
+
 
 # try:
 #     df = pd.read_csv(r"C:\Users\A4952\Desktop\Contact_info_t2.csv", nrows=10)
@@ -21,14 +32,16 @@ a=10
 #     print("File is not found in specified location", E)
 #     df = pd.read_csv(r"C:\Users\A4952\Desktop\Contact_info_t.csv", nrows=5)
 
-try:
-    with open(r"C:\Users\A4952\Desktop\text_file.txt", 'r') as f:
-        file = f.read()
-        print(file)
-        f.close()
-except:
-    print("file was not present")
-finally:
-    print("finally printing")
+# try:
+#     with open(r"C:\Users\A4952\Desktop\text_file2.txt", 'r') as f:
+#         file = f.read()
+#         print(file)
+#         f.close()
+#
+# except FileNotFoundError as e:
+#     print("file was not present", e)
+# finally:
+#     print("finally printing")
+
 
 
