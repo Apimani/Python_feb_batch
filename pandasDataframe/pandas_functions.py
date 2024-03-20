350616972cd43826c364ff6fe10a2245cf3222f6
Sreeni <kattubadi426@gmail.com>
2024-03-20 10:03:25 +0530

    new commit

--- pandasDataframe/pandas_functions.py
+++ pandasDataframe/pandas_functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandasql import sqldf
 
 data = {'id':[1,2,3,4,5,6,1,2,2,2], 'marks':[89,23,12,17,18,90,87,89,11,90]}
 
@@ -53,8 +54,24 @@ join['prod_code_left']= join['prod_code_left'].fillna('999')
 join['prod_code_right']= join['prod_code_right'].fillna('UNKNWN')
 
 print(join)
-
 df = pd.read_csv(r"C:\Users\A4952\Downloads\Python_feb_batch-20240218T160531Z-001\Python_feb_batch\Files\Employee.csv")
+# print(df)
+# print(df.iloc[2:7,])
+# print(df.iloc[: , 0:2])
+# print(df[['Identifier','Surname']])
+# print(df)
+# print(df.loc[1:5,'Identifier':'birthmonth'])
+#
+# print(df[((df.Identifier == 6 ) & (df.Surname == 'Raheem')) | (df.birthmonth >=198904 )])
+
+
+print(sqldf("""select identifier, count(1) as cnt from df 
+             group by identifier """))
+
+
+
+
+
 
 
 
